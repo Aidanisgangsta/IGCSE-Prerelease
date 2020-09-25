b9b726ed30ec502ec5327539b0eaa5355ee1c572
Aidanisgansta <aidanjw05@gmail.com>
2020-09-25 12:31:40 +1200

    Update main.py
    Fixed stuff

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ customer_choice = {
     "Salad3": None
     }
 
-print("Hello customer, please pick one of the following options for Baguette size:")
-
+print("Hello customer, please enter one of the following options for Baguette size:")
 #Finds the customers baguette size
 while True:
     for i in range(0, len(Baguette_Size)):
-        if i > 0:            
+        if i > 0:
             print(", ", end="")
-        elif i == Baguette_Size:
-            print("")
         print(Baguette_Size[i], end="")
+    print("")
     size = input()
+
     if size in Baguette_Size:
         print(f"Ok you have selected a {size} baguette\n")
         customer_choice.update({"Size": size})
@@ -30,17 +29,20 @@ while True:
     else:
         print("Please select a valid option")
 
+print("Please now select you bread type out of the following:")
 #Finds the customers bread
 while True:
     for i in range(0, len(Baguette_Bread)):
         if i > 0:            
             print(", ", end="")
         print(Baguette_Bread[i], end="")
-        bread = input()
-        if bread in Baguette_Bread:
-            print(f"Ok you have selected a {bread} baguette\n")
-            customer_choice.update({"Bread": bread})
-            break
+    print("")
+    bread = input()
+
+    if bread in Baguette_Bread:
+        print(f"Ok you have selected a {bread} baguette\n")
+        customer_choice.update({"BreadType": bread})
+        break
     else:
         print("Please select a valid option")
 
